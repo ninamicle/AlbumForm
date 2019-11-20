f32aa17679cd11904a34e823b814f1be13785ab6
NINA_MICLEUSANU <NINA.MICLEUSANU93@gmail.com>
2019-11-20 08:45:24 +0100

    First commit

--- src/componenti/LoginForm.js
+++ src/componenti/LoginForm.js
@@ -1,126 +1,141 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./LoginForm.css";
-import {Link} from 'react-router-dom';
 
 
-
-const formValid = ({ formErrors, ...rest }) => {
-  let valid = true;
-
-  // validate form errors being empty
-  Object.values(formErrors).forEach(val => {
-    val.length > 0 && (valid = false);
-  });
-
-  // validate the form was filled out
-  Object.values(rest).forEach(val => {
-    val === null && (valid = false);
-  });
-
-  return valid;
+const txtFieldState = {
+  value: "",
+  valid: true,
+  typeMismatch: false,
+  errMsg: "" //this is where our error message gets across
 };
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: null,
-      password: null,
-      formErrors: {
-        username: "",
-        password: ""
-      }
-    };
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    if (formValid(this.state)) {
-      alert(`
-        --SUBMITTING--
-        Username: ${this.state.username}
-        Password: ${this.state.password}
-      `);
-    } else {
-      alert("FORM INVALID - DISPLAY ERROR MESSAGE");
-    }
+  state = {
+    username: {
+      ...txtFieldState,
+      fieldName: "Username",
+      required: true,
+      requiredTxt: "Username is required"
+    },
+    password: {
+      ...txtFieldState,
+      fieldName: "Password",
+      required: false,
+      requiredTxt: "Password is required"
+    },
+    allFieldsValid: false
   };
 
-  handleChange = e => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
-
-    switch (name) {
-      case "username":
-        formErrors.username =
-          value.length < 3 ? "minimum 3 characaters required" : "";
-        break;
-      case "password":
-        formErrors.password =
-          value.length < 6 ? "minimum 6 characaters required" : "";
-        break;
-      default:
-        break;
-    }
-
-    //this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  //we need to extract specific properties in Constraint Validation API using this code snippet
+  reduceFormValues = formElements => {
+    const arrElements = Array.prototype.slice.call(formElements); //we convert elements/inputs into an array found inside form element
+    //we need to extract specific properties in Constraint Validation API using this code snippet
+    const formValues = arrElements
+      .filter(elem => elem.name.length > 0)
+      .map(x => {
+        const { typeMismatch } = x.validity;
+        const { name, type, value } = x;
+        return {
+          name,
+          type,
+          typeMismatch, //we use typeMismatch when format is incorrect(e.g. incorrect email)
+          value,
+          valid: x.checkValidity()
+        };
+      })
+      .reduce((acc, currVal) => {
+        //then we finally use reduce, ready to put it in our state
+        const { value, valid, typeMismatch } = currVal;
+        const { fieldName, requiredTxt, formatErrorTxt } = this.state[
+          currVal.name
+        ]; //get the rest of properties inside the state object
+        //we'll need to map these properties back to state so we use reducer...
+        acc[currVal.name] = {
+          value,
+          valid,
+          typeMismatch,
+          fieldName,
+          requiredTxt,
+          formatErrorTxt
+        };
+        return acc;
+      }, {});
+    return formValues;
   };
+  checkAllFieldsValid = formValues => {
+    return !Object.keys(formValues)
+      .map(x => formValues[x])
+      .some(field => !field.valid);
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const formValues = this.reduceFormValues(form.elements);
+    const allFieldsValid = this.checkAllFieldsValid(formValues);
+
+    //note: put ajax calls here to persist the form inputs in the database.
+    //END
+    this.setState({ ...formValues, allFieldsValid }); //we set the state based on the extracted values from Constraint Validation API
+  };
+  
 
   render() {
-    const { formErrors } = this.state;
-
-    return (
-      <div className="wrapper">
+    const ErrorValidationLabel = ({ txtLbl }) => (
+      <label htmlFor="" style={{ color: "red" }}>
+        {txtLbl}
+      </label>
+    );
+    const {username, password, allFieldsValid } = this.state;
+    const successFormDisplay = allFieldsValid ? "block" : "none";
+    const inputFormDisplay = !allFieldsValid ? "block" : "none";
+//const renderEmailValidationError = email.valid ? 
+       // "" : 
+        //<ErrorValidationLabel txtLbl={email.typeMismatch ? email.formatErrorTxt : email.requiredTxt} />;
+    const renderDateValidationError = username.valid ? "" : <ErrorValidationLabel txtLbl={username.requiredTxt} />;
+    const renderFnameValidationError = password.valid ? "" : <ErrorValidationLabel txtLbl={password.requiredTxt} />;
+return (
+  <div className="wrapper">
         <div className="form-wrapper">
             <div>
                 <img src="http://cdn.shopify.com/s/files/1/1756/9559/products/pokeball_coaster_photo_33c69500-8564-4842-a2a7-3803975a2d3b_1024x1024.jpg?v=1557064432" alt="pokeball"/>
             </div>
           <h1>Login</h1>
-          <form onSubmit={this.handleSubmit} noValidate>
-            <div className="username">
-              <label htmlFor="username">Username</label>
-              <input
-                className={formErrors.username.length > 0 ? "error" : null}
-                placeholder="Username"
-                type="text"
-                name="username"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.username.length > 0 && (
-                <span className="errorMessage">{formErrors.username}</span>
-              )}
-            </div>
-           <div className="password">
-              <label htmlFor="password">Password</label>
-              <input
-                className={formErrors.password.length > 0 ? "error" : null}
-                placeholder="Password"
-                type="password"
-                name="password"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.password.length > 0 && (
-                <span className="errorMessage">{formErrors.password}</span>
-              )}
-            </div>
-            <div className="login">
-            <Link to='./MyProfile' >
-                <button  type='submit' >Login</button>
-            </Link>
-            </div>
-             
 
-          </form>
+        <>
+            <div style={{display: successFormDisplay}}>
+                <h1 style={{ textAlign: "center" }}>Success!</h1>
+                <p style={{ textAlign: "center" }}>
+                    You have successfully Login.
+                </p>
+            </div>
+            <div className="form-input" style={{display: inputFormDisplay}}>
+                
+                <form
+                    className="form-inside-input"
+                    onSubmit={this.onSubmit}
+                    noValidate
+                >
+                    <input className='username' type="text" name="username" placeholder="Username" required />
+                    <br />
+                    {renderDateValidationError}
+                    <br />
+                    <input className='password' type="password" name="password" placeholder="Password" required />
+                    <br />
+                    {renderFnameValidationError}
+                    <br />
+                    
+                    <div>
+                    <input className='button' type="submit" value="Submit"></input>
+                    </div>
+                </form>
+            </div>
+        </>
         </div>
-      </div>
-      
+        </div>
     );
-  }
+}
 }
 
 export default App;
